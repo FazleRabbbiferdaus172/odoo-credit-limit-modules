0d5922945315a16fa18c8ff0387f6ab72b0ba5a1
FazleRabbbiferdaus172 <fazle.ferdaus1416@gmail.com>
2025-09-23 16:53:33 -0500

    [Init] initial scafflod of addon

---
+++ sale_credit_limit_block/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Sale Credit Limit Block",
+
+    'summary': "This module blocks confirming sale order when customer has passed credit limit",
+
+    'description': """
+                This module blocks confirming sale order when customer has passed credit limit.
+    """,
+
+    'author': "Fazle Rabbi Ferdaus",
+
+    'category': 'Sales Management',
+    'version': '18.0.1.0.0',
+
+    'depends': ['base'],
+
+    'data': [
+        # 'security/ir.model.access.csv',
+    ],
+}
