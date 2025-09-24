6d0ba7005e5cf8b1885f95c7500373ef29850e55
FazleRabbbiferdaus172 <fazle.ferdaus1416@gmail.com>
2025-09-23 22:17:06 -0500

    [IMP] method to calculate overdue amount implemented
    all expected test cases pass

---
+++ sale_credit_limit_block/models/res_partner.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    def _get_overdue_amount(self):
+        self.ensure_one()
+        overdue_invoice_domain = [
+            ('state', 'not in', ('cancel', 'draft')),
+            ('move_type', '=', 'out_invoice'),
+            ('payment_state', 'not in', ('in_payment', 'paid', 'reversed', 'blocked', 'invoicing_legacy')),
+            ('invoice_date_due', '<', fields.Date.today()),
+            ('partner_id', '=', self.id),
+        ]
+        overdue_invoices = self.env['account.move'].search(overdue_invoice_domain)
+        overdue_amount = sum(overdue_invoices.mapped('amount_residual'))
+        return overdue_amount
